3467158a24b602fc3ac00ae315a36188066dabe9
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2026-04-24 16:15:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,28 @@ const projects = await fetch("./data/trending.json");
 const data = await projects.json();
 
 function addProject(project) {
+    const projectUrl = `https://scratch.mit.edu/projects/${project.id}/`;
+    const userUrl = `https://scratch.mit.edu/users/${project.username}/`;
     container.insertAdjacentHTML("beforeend", `
-            <a href="https://scratch.mit.edu/projects/${project.id}/" class="card" target="_blank" rel="noopener noreferrer">
+            <div class="card">
                 <div class='background' style="background-image: linear-gradient(180deg,rgba(255, 255, 255, 0) 0%, rgb(0, 0, 0) 100%), url(${project.banner})"></div>
-                <img class="card-image" src="${project.banner}">
+                <a class="link" href="${projectUrl}" target="_blank" rel="noopener noreferrer">
+                    <img class="card-image" src="${project.banner}">
+                </a>
                 <div class="profile">
-                    <img class="profile-picture" src="${project.avatar}">
+                    <a class="link" href="${userUrl}" target="_blank" rel="noopener noreferrer">
+                        <img class="profile-picture" src="${project.avatar}">
+                    </a>
                     <div class="profile-text">
-                        <h2 class="card-header">${DOMPurify.sanitize(project.title)}</h2>
-                        <p class="card-author">${project.username}</p>
+                        <a href="${projectUrl}" target="_blank" rel="noopener noreferrer">
+                            <h2 class="card-header">${DOMPurify.sanitize(project.title)}</h2>
+                        </a>
+                        <a href="${userUrl}" target="_blank" rel="noopener noreferrer">
+                            <p class="card-author">${project.username}</p>
+                        </a>
                     </div>
                 </div>
-            </a>
+            </div>
         `);
 }
 
